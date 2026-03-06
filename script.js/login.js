54c13851afaec4102da06962f0374244fbaaff79
mubasshir <mubasshirkamali2009@gmail.com>
2026-03-07 00:07:23 +0600

    first commit

---
+++ script.js/login.js
@@ -0,0 +1,20 @@
+const usernameInput=document.getElementById( "input-username" );
+const  passwordInput=document.getElementById("input-password");
+
+
+
+
+document.getElementById("login-btn" ).addEventListener("click",
+    function(){
+        const user=usernameInput.value;
+        const pass=passwordInput.value;
+        if(user=='admin' && pass=='admin123'){
+            alert('Login success')
+            window.location.assign("/home.html")
+        }
+        else{
+            alert("Youre Not Allowed Try Agine");
+            return;
+        }
+    }
+)
