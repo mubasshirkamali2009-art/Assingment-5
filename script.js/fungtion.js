b75ab7ee9a6ac0cbedad8b30dbcb452b215edeea
mubasshir <mubasshirkamali2009@gmail.com>
2026-03-09 00:14:51 +0600

    daynamic modal  done

--- script.js/fungtion.js
+++ script.js/fungtion.js
@@ -1,13 +1,13 @@
-const issuesContainer=document.getElementById("issues-container");
-const loadingspener=document.getElementById("spiner");
-const allIssuesBtn=document.getElementById("all-issues-btn");
-const openIssuesbtn=document.getElementById("open-issues-btn");
-const closedIssueBtn=document.getElementById("close-issues-btn");
-const allbtn=
-document.querySelectorAll("#close-issues-btn, #open-issues-btn, #all-issues-btn")
-const issueCounter=document.getElementById("Issues-counter");
-const modal         = document.getElementById("issue-modal");
-const modalOverlay  = document.getElementById("modal-overlay");
+const issuesContainer = document.getElementById("issues-container");
+const loadingspener = document.getElementById("spiner");
+const allIssuesBtn = document.getElementById("all-issues-btn");
+const openIssuesbtn = document.getElementById("open-issues-btn");
+const closedIssueBtn = document.getElementById("close-issues-btn");
+const allbtn =
+  document.querySelectorAll("#close-issues-btn, #open-issues-btn, #all-issues-btn")
+const issueCounter = document.getElementById("Issues-counter");
+const modal = document.getElementById("issue-modal");
+const modalOverlay = document.getElementById("modal-overlay");
 const modalCloseBtn = document.getElementById("modal-close-btn");
 
 
@@ -15,114 +15,150 @@ const modalCloseBtn = document.getElementById("modal-close-btn");
 let allIssues = [];
 
 
-loadmodal=async(id)=>{
-const url=`https://phi-lab-server.vercel.app/api/v1/lab/issues/${id}`;
-console.log(url)
-const  res=await fetch(url);
-const details=await res.json()
-displayissuedetail(details.data)
+loadmodal = async (id) => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/${id}`;
+  console.log(url)
+  const res = await fetch(url);
+  const details = await res.json()
+  displayissuedetail(details.data)
 
 }
 
 
 
 
-function showloading(){
+function showloading() {
   loadingspener.classList.remove("hidden");
   loadingspener.classList.add("flex");
 }
-function  hiddenloading(){
-   loadingspener.classList.add("hidden")
-    loadingspener.classList.remove("flex");
+function hiddenloading() {
+  loadingspener.classList.add("hidden")
+  loadingspener.classList.remove("flex");
 }
 
-async function  loadissues(){
-    showloading()
-    setTimeout(async() => {
-      const res=await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
-    const data=await  res.json();
+async function loadissues() {
+  showloading()
+  setTimeout(async () => {
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    const data = await res.json();
     hiddenloading()
-      allIssues = data.data;
+    allIssues = data.data;
     displayissues(allIssues);
-    }, 1000);
-     
+  }, 1000);
+
 }
 
-const loadissuedetail= async(id)=>{
-  const  url= `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
-  const res= await fetch(url);
-  const  details=await res.json();
-   displayIsuueDetails(details.data)
+const loadissuedetail = async (id) => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+  const res = await fetch(url);
+  const details = await res.json();
+
+  displayIsuueDetails(details.data)
+}
+const detailBox = document.getElementById("details-container");
+const displayIsuueDetails = (issue) => {
+  console.log(issue)
+
+  detailBox.innerHTML = ` <h2 class="font -bold text-2xl text-black">${issue.title}</h2>
+<div class="flex gap-5  ">
+  <div class="badge  ${issue.status === 'open' ?
+  'badge-success' : 'badge-primary'  } ">${issue.status}
+</div>
+<div>
+
+<p class="text-gray-500"><i class="fa-solid fa-circle w-1 h-1"></i> Opened my ${issue.author} <i class="fa-solid fa-circle w-1 h-1"></i> ${issue.updatedAt}</p>
+</div>
+
+</div>
+<div class="flex gap-2 flex-wrap mb-4  py-7">
+      ${issue.labels.map(label => `
+        <span class="text-xs px-2 py-1 rounded-full border 
+         font-medium ${label === 'help wanted' ? 'border-orange-300 text-orange-500 bg-orange-100'
+          : label === 'bug' ? 'border-red-300 text-red-500 bg-red-100'
+            : label === 'documentation' ? 'border-yellow-300 text-green-yellow bg-yellow-100'
+
+              : label === 'enhancement' ? 'border-green-300 text-green-500 bg-green-100'
+                : label === 'good first issue' ? 'border-purple-300 text-purple-500 bg-purple-100 '
+                  : 'border-gray-300 text-gray-500 bg-gray-100'
+        }">
+      
+          ${label}
+        </span>
+      `).join("")}
+    </div>
+    <p class="text-gray-500 pb-8">${issue.description}</p>
+    <div class="flex justify-around">
+      <p class="text-gray-500">Assignee: <br><span class="font-extrabold text-xl text-black">${issue.author}</span></p>
+      <p class="text-gray-500">Priority <br><span class="text-xs font-bold px-3 py-1 rounded-full border  ${issue.priority === 'high' ? 'border-red-300 text-red-500 bg-red-100'
+        : issue.priority === 'medium' ? 'border-orange-300 text-orange-500 bg-orange-100 '
+          : 'border-gray-300 text-gray-500 bg-gray-100'
+      }">${issue.priority}</span></p>
+    </div>
+  `;
+  console.log(detailBox)
+  document.getElementById("issue_modal").showModal();
 }
 
-  const displayIsuueDetails=(issue)=>{
-console.log(issue)
-const detailBox=document.getElementById("details-container");
-detailBox.innerHTML="hello"
-document.getElementById("issue_modal").showModal();
-  }
 
-
-function displayAllIssues() { 
-    showloading()
-    allbtn.forEach(btn=>{
+function displayAllIssues() {
+  showloading()
+  allbtn.forEach(btn => {
     btn.classList.remove("btn-primary")
-})
-      setTimeout(() => {
-         allIssuesBtn.classList.add("btn-primary")
-         hiddenloading()
-  displayissues(allIssues);
-      }, 500);
+  })
+  setTimeout(() => {
+    allIssuesBtn.classList.add("btn-primary")
+    hiddenloading()
+    displayissues(allIssues);
+  }, 500);
 }
 
-function displayOpenIssues(){
-    showloading()
-     allbtn.forEach(btn=>{
+function displayOpenIssues() {
+  showloading()
+  allbtn.forEach(btn => {
     btn.classList.remove("btn-primary")
-})
-         openIssuesbtn.classList.add("btn-primary")
-setTimeout(() => {
-  const  openIssues=allIssues.filter(issue =>  issue.status==="open");
-hiddenloading()
-displayissues(openIssues)
-}, 500);
+  })
+  openIssuesbtn.classList.add("btn-primary")
+  setTimeout(() => {
+    const openIssues = allIssues.filter(issue => issue.status === "open");
+    hiddenloading()
+    displayissues(openIssues)
+  }, 500);
 
 }
 
 
-function   displayClosedIssues(){
-    showloading()
-     allbtn.forEach(btn=>{
+function displayClosedIssues() {
+  showloading()
+  allbtn.forEach(btn => {
     btn.classList.remove("btn-primary")
-})
-         closedIssueBtn.classList.add("btn-primary")
-    setTimeout(() => {
-      const  closedissues=allIssues.filter(issue =>issue.status==="closed");
+  })
+  closedIssueBtn.classList.add("btn-primary")
+  setTimeout(() => {
+    const closedissues = allIssues.filter(issue => issue.status === "closed");
     hiddenloading()
     displayissues(closedissues)
-    }, 500);
+  }, 500);
 }
 
-function  displayissues(issues){
-    issuesContainer.innerHTML="";
-      issueCounter.innerHTML = issues.length;
-issues.forEach( issue=> {
-   
-      
-    const  statusbasedimg=issue.status==="open"
-    ? "./assets/Open-Status.png"
-    :  "./assets/Closed- Status .png"
+function displayissues(issues) {
+  issuesContainer.innerHTML = "";
+  issueCounter.innerHTML = issues.length;
+  issues.forEach(issue => {
+
+
+    const statusbasedimg = issue.status === "open"
+      ? "./assets/Open-Status.png"
+      : "./assets/Closed- Status .png"
     console.log(issue)
- 
-    const card=document.createElement("div");
-    card.innerHTML=` <div    class="bg-white rounded-2xl h-[300px]  ${issue.status=='open'?'border-t-3 border-green-500' :'border-t-3 border-red-500' } p-5 w-80 shadow-sm">
+
+    const card = document.createElement("div");
+    card.innerHTML = ` <div   onclick="loadissuedetail(${issue.id})" class="bg-white rounded-2xl h-[300px]  ${issue.status == 'open' ? 'border-t-3 border-green-500' : 'border-t-3 border-red-500'} p-5 w-80 shadow-sm">
     <div class="flex justify-between items-center           mb-3">
       <img class="w-7 h-7" src="${statusbasedimg}" alt="${issue.status}">
-      <span class="text-xs font-bold px-3 py-1 rounded-full border  ${ 
-      issue.priority === 'high'   ? 'border-red-300 text-red-500 bg-red-100'   
-      : issue.priority === 'medium'? 'border-orange-300 text-orange-500 bg-orange-100 '
-      : 'border-gray-300 text-gray-500 bg-gray-100' 
-    }">${issue.priority}</span>
+      <span class="text-xs font-bold px-3 py-1 rounded-full border  ${issue.priority === 'high' ? 'border-red-300 text-red-500 bg-red-100'
+        : issue.priority === 'medium' ? 'border-orange-300 text-orange-500 bg-orange-100 '
+          : 'border-gray-300 text-gray-500 bg-gray-100'
+      }">${issue.priority}</span>
     </div>
     <h2 onclick="loadissuedetail(${issue.id})" class="font-bold text-gray-900 text-base mb-2">${issue.title}</h2>
     <p class="text-gray-500 text-sm mb-4">${issue.description}</p>
@@ -131,14 +167,14 @@ issues.forEach( issue=> {
     <div class="flex gap-2 flex-wrap mb-4">
       ${issue.labels.map(label => `
         <span class="text-xs px-2 py-1 rounded-full border 
-         font-medium ${label === 'help wanted'   ? 'border-orange-300 text-orange-500 bg-orange-100' 
-      : label === 'bug'           ? 'border-red-300 text-red-500 bg-red-100'   
-      : label === 'documentation' ? 'border-yellow-300 text-green-yellow bg-yellow-100'  
-     
-        : label === 'enhancement'    ? 'border-green-300 text-green-500 bg-green-100'  
-      : label === 'good first issue'? 'border-purple-300 text-purple-500 bg-purple-100 '
-      : 'border-gray-300 text-gray-500 bg-gray-100' 
-    }">
+         font-medium ${label === 'help wanted' ? 'border-orange-300 text-orange-500 bg-orange-100'
+          : label === 'bug' ? 'border-red-300 text-red-500 bg-red-100'
+            : label === 'documentation' ? 'border-yellow-300 text-green-yellow bg-yellow-100'
+
+              : label === 'enhancement' ? 'border-green-300 text-green-500 bg-green-100'
+                : label === 'good first issue' ? 'border-purple-300 text-purple-500 bg-purple-100 '
+                  : 'border-gray-300 text-gray-500 bg-gray-100'
+        }">
       
           ${label}
         </span>
@@ -148,31 +184,31 @@ issues.forEach( issue=> {
     <hr class="border-gray-100 mb-3">
     <p class="text-gray-400 text-xs">#${issue.id} by <span class="font-medium">${issue.author}</span></p>
     <p class="text-gray-400 text-xs mt-1">${issue.createdAt}</p>
-  </div>` 
+  </div>`
 
 
-issuesContainer.appendChild(card);
-});
+    issuesContainer.appendChild(card);
+  });
 
 }
 
 loadissues()
 
-document.getElementById("search-input-btn").addEventListener("click", 
-  ()=>{
-    allbtn.forEach(btn=>{
-    btn.classList.remove("btn-primary")
-})
-const  inputSearch=document.getElementById("search-input");
-const  searchValue=inputSearch.value.trim().toLowerCase();
-console.log(searchValue)
-fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`)
-.then((res)=>res.json())
-.then((data)=>{
-  const allIssues=data.data;
-  console.log(allIssues)
-  const filterIssues=allIssues.filter(issue=>issue.title.toLowerCase().includes(searchValue));
-  displayissues(filterIssues);
-});
+document.getElementById("search-input-btn").addEventListener("click",
+  () => {
+    allbtn.forEach(btn => {
+      btn.classList.remove("btn-primary")
+    })
+    const inputSearch = document.getElementById("search-input");
+    const searchValue = inputSearch.value.trim().toLowerCase();
+    console.log(searchValue)
+    fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const allIssues = data.data;
+        console.log(allIssues)
+        const filterIssues = allIssues.filter(issue => issue.title.toLowerCase().includes(searchValue));
+        displayissues(filterIssues);
+      });
 
-})
+  })
